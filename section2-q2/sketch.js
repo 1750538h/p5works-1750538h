fa18d609f585bb8ea50c31d8e81c6766eded70f7
1750538h <soratiat@gmail.com>
2020-10-27 12:32:03 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,13 +6,31 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-      rect(size * i, size * j, size, size);
-       if((i+j) % 2 == 1){
-        fill(196);
+      if((i+j)%2==1){
+       fill(128);
        }
-     else{
-        fill(255);
+    else{
+       fill(255);
+   }
+      rect(size*i, size*j, size, size);
+
+
+      }
+  }
+  for(let x =0;x<8; x++){
+    for(let y =0; y < 3; y++){
+      if((x+y)%2==1){
+        fill(255,0,0);
+        ellipse(25*x+12.5,25*y+12.5,size*0.9,size*0.9);
+      }
     }
-     }
+  }
+  for(let a =0;a<8; a++){
+    for(let b =5; b < 8; b++){
+      if((a+b)%2==1){
+        fill(0);
+        ellipse(25*a+12.5,25*b+12.5,size*0.9,size*0.9);
+      }
+    }
   }
 }
