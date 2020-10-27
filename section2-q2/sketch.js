ac9d6291f9766900b15f648b808dac18ce725add
1750538h <soratiat@gmail.com>
2020-10-27 10:58:50 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -2,22 +2,15 @@
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
-  //noStroke();
+  noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       rect(size * i, size * j, size, size);
-       if((i+j) % 2 == 0 && j<4){
+       if((i+j) % 2 == 1){
         fill(196);
-        //fill(255,0,0);
-        //elipse(size*i, size*j size);
        }
-       else if((i+j)%2==0 && j<4){
-         fill(144);
-       }
-       else if((i+j)%2==0){
-         fill(14);
-       }
+
      else{
         fill(255);
     }
