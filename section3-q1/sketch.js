d9a93c63b3b5ce3024ac9bb13be14adc42ac81d1
1750538h <soratiat@gmail.com>
2020-11-10 12:16:22 +0900

    Change section3-q1
    位置変更と吹き出しの色変え

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,19 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",93,100,150);
 }
 
-function balloon(t){
+function balloon(t,n,x,y){
   let w = textWidth(t);
-  let h = textAscent() + textDescent();
+  let h = textAscent(t) + textDescent(t);
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  noStroke();
+  fill(n);
+  rect(x, y, w + p * 2, h + p * 2);
+  noStroke();
+  fill(n);
+  triangle(x+w+p*2 ,+y+h+p*2-10, x+w+p*2, y+h+p*2, x+w+p*2+30, y+h+p*2);
   fill(255);
-  text(t, p, h + p);
+  text(t, x+p, y+h + p);
 }
