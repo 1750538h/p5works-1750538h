6f998265286ba4a1a5505b6895997f0e6c0c1991
1750538h <soratiat@gmail.com>
2020-11-17 11:34:04 +0900

    Change section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,16 +1,31 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size = 25;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  if(count<50){
+    noStroke();
+   fill(255,0,0);
+   size=size+1
   ellipse(width / 2, height / 2, size);
+}
+  else{
+    size=size-1
+    ellipse(width/2, height/ 2, size);
+  }
+}
+
+function draw(){
+  background(160, 192, 255);
+
 }
